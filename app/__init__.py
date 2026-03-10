334847e6f54708c83a75091033b9b7ab1953ac51
Babi Thakur <babi@localhost.localdomain>
2026-03-10 18:32:45 +0545

    Some refactor

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,3 @@
-# from flask import Flask
-
-# def create_app():
-#     app = Flask(__name__)
-
-#     # Register blueprints
-#     from app.api.routes.main_routes import main_bp
-#     app.register_blueprint(main_bp)
-
-#     return app
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
@@ -21,7 +10,7 @@ def create_app():
 
     db.init_app(app)
 
-    # Register blueprints
+    #register blueprints
     from app.api.routes.user_routes import user_bp
     from app.api.routes.report_routes import report_bp
     app.register_blueprint(user_bp)
