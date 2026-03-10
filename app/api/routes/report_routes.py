80be2d1b63ffbd4cb0a9e19d16469d1dce6af5bc
Babi Thakur <babi@localhost.localdomain>
2026-03-10 19:49:30 +0545

    Added frontend design for blur image analysis

--- app/api/routes/report_routes.py
+++ app/api/routes/report_routes.py
@@ -5,3 +5,7 @@ report_bp = Blueprint("report", __name__)
 @report_bp.route("/validate")
 def validate():
     return render_template("validate.html")
+
+@report_bp.route("/blur_analysis")
+def blur_analysis():
+    return render_template("blur_analysis.html", variance_value=369.98, threshold=100)
